9984413e9abb4ab47a59bd4f81f803ee5b5a2507
Ricardo Amarilla <amarillaricardo@users.noreply.github.com>
2024-02-28 19:31:44 -0300

    Update main.py

--- mi_app/main.py
+++ mi_app/main.py
@@ -10,12 +10,12 @@ from excepciones import RegistroLogError
 from excepciones import registrar
 import observador
 
-if __name__=="__main__":
+if __name__ == "__main__":
     root_tk = Tk()
     mi_app = VistaPrincipal(root_tk)
     observador_de_simulaciones_de_cotizaciones_de_fondos = observador.ConcreteObserverA(mi_app.mi_modelo)
     root_tk.mainloop()
 try:
-   registrar()
+    registrar()
 except RegistroLogError as log:
-   log.registrar_error()
+    log.registrar_error()

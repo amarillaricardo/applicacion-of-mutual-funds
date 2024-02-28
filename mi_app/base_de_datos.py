379f7469ae6b43fa2a5daeb9148716499c81aea1
Ricardo Amarilla <amarillaricardo@users.noreply.github.com>
2024-02-28 18:55:34 -0300

    Update base_de_datos.py

--- mi_app/base_de_datos.py
+++ mi_app/base_de_datos.py
@@ -151,50 +151,54 @@ class BaseDeDatos():
         item_del_treeview = tree.focus()
         id_del_fondo_a_modificar = str(tree.item(item_del_treeview)['text'])
         if item_del_treeview == "":
-            showinfo("Treeview", "Debe seleccionar un fondo de la tabla de abajo para continuar, luego de haber consultado la ddbb. Gracias")
+            showinfo("Treeview", 
+                     "Debe seleccionar un fondo de la tabla de abajo para continuar, luego de haber consultado la ddbb. Gracias")
         elif item_del_treeview != "":
             if askyesno("Modificar registro de la ddbb", "¿Esta seguro que desea modificar el registro?"):
-                if nombre_del_fondo != "" and tipo_de_inversion != "" and sociedad_gerente != "" and sociedad_depositaria != "" and codigo_cafci != "":
-                    con = self.conexion()
-                    cursor = con.cursor()
-                    data = (nombre_del_fondo, tipo_de_inversion, horizonte, sociedad_gerente,
-                            sociedad_depositaria, region, cotizado_originalmente, calificacion,
-                            fecha_de_calificacion, calificadora_de_riesgo, pais_sede,
-                            tipo_de_activo, estado, bolsa, codigo_cafci, comision_de_ingreso,
-                            honorarios_de_administracion, comision_de_egreso, comision_de_transferencia,
-                            gastos_ordinarios_de_gestion, cobra_comision_por_desempeno,
-                            inversion_minima, plazo_de_liquidacion)
-                    sql = """UPDATE fondos
-                           SET nombre_del_fondo =?,
-                               tipo_de_inversion = ?,
-                               horizonte = ?,
-                               sociedad_gerente = ?,
-                               sociedad_depositaria  = ?,
-                               region  = ?,
-                               cotizado_originalmente  = ?,
-                               calificacion  = ?,
-                               fecha_de_calificacion  = ?,
-                               calificadora_de_riesgo  = ?,
-                               pais_sede  = ?,
-                               tipo_de_activo  = ?,
-                               estado  = ?,
-                               bolsa  = ?,
-                               codigo_cafci  = ?,
-                               comision_de_ingreso  = ?,
-                               honorarios_de_administracion  = ?,
-                               comision_de_egreso  = ?,
-                               comision_de_transferencia  = ?,
-                               gastos_ordinarios_de_gestion  = ?,
-                               cobra_comision_por_desempeno  = ?,
-                               inversion_minima  = ?,
-                               plazo_de_liquidacion  = ?
-                           WHERE id = """ + id_del_fondo_a_modificar
-                    cursor.execute(sql, data)
-                    con.commit()
-                    self.actualizar_treeview(tree)
-                    return f"Se ha modificado el fondo con el id {id_del_fondo_a_modificar} con los siguiente datos: {data}"
-                    showinfo("Base de datos: Actualizacion",
-                             "Se ha modificado el registro con id " + id_del_fondo_a_modificar + " con exito")
+                if nombre_del_fondo != "" and tipo_de_inversion != "" and sociedad_gerente != "":
+                    if sociedad_depositaria != "" and codigo_cafci != "":
+                        con = self.conexion()
+                        cursor = con.cursor()
+                        data = (nombre_del_fondo, tipo_de_inversion, horizonte, sociedad_gerente,
+                                sociedad_depositaria, region, cotizado_originalmente, calificacion,
+                                fecha_de_calificacion, calificadora_de_riesgo, pais_sede,
+                                tipo_de_activo, estado, bolsa, codigo_cafci, comision_de_ingreso,
+                                honorarios_de_administracion, comision_de_egreso, comision_de_transferencia,
+                                gastos_ordinarios_de_gestion, cobra_comision_por_desempeno,
+                                inversion_minima, plazo_de_liquidacion)
+                        sql = """UPDATE fondos
+                               SET nombre_del_fondo =?,
+                                   tipo_de_inversion = ?,
+                                   horizonte = ?,
+                                   sociedad_gerente = ?,
+                                   sociedad_depositaria  = ?,
+                                   region  = ?,
+                                   cotizado_originalmente  = ?,
+                                   calificacion  = ?,
+                                   fecha_de_calificacion  = ?,
+                                   calificadora_de_riesgo  = ?,
+                                   pais_sede  = ?,
+                                   tipo_de_activo  = ?,
+                                   estado  = ?,
+                                   bolsa  = ?,
+                                   codigo_cafci  = ?,
+                                   comision_de_ingreso  = ?,
+                                   honorarios_de_administracion  = ?,
+                                   comision_de_egreso  = ?,
+                                   comision_de_transferencia  = ?,
+                                   gastos_ordinarios_de_gestion  = ?,
+                                   cobra_comision_por_desempeno  = ?,
+                                   inversion_minima  = ?,
+                                   plazo_de_liquidacion  = ?
+                               WHERE id = """ + id_del_fondo_a_modificar
+                        cursor.execute(sql, data)
+                        con.commit()
+                        self.actualizar_treeview(tree)
+                        return f"Se ha modificado el fondo con el id {id_del_fondo_a_modificar} con los siguiente datos: {data}"
+                        showinfo("Base de datos: Actualizacion",
+                                 "Se ha modificado el registro con id " + id_del_fondo_a_modificar + " con exito")
+                        else:
+                    showerror("Error en campos obligatorios", "Es necesario completar el/los campo/os obligatorio/os(*)")
                 else:
                     showerror("Error en campos obligatorios", "Es necesario completar el/los campo/os obligatorio/os(*)")
             else:

363fd691e44307a9edb11fb63ddb0f2b363aaefa
Ricardo Amarilla <amarillaricardo@users.noreply.github.com>
2024-02-28 19:12:51 -0300

    Update base_de_datos.py

--- mi_app/base_de_datos.py
+++ mi_app/base_de_datos.py
@@ -196,7 +196,8 @@ class BaseDeDatos():
                         cursor.execute(sql, data)
                         con.commit()
                         self.actualizar_treeview(tree)
-                        return f"Se ha modificado el fondo con el id {id_del_fondo_a_modificar} con los siguiente datos: {data}"
+                        return f"Se ha modificado el fondo con el id {id_del_fondo_a_modificar} " + 
+                                "con los siguiente datos: {data}"
                         showinfo("Base de datos: Actualizacion",
                                  "Se ha modificado el registro con id " +
                                  id_del_fondo_a_modificar + " con exito")
